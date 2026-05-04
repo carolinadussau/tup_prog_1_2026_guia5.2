47cba9fc1c2ec881341c39a2ec7678012a50f993
Carolina Dussau <Carolina.Dussau@endava.com>
2026-05-03 22:13:28 -0300

    ejercicio 4

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayorValor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar variables
+            double num, numMayor;
+
+            // Primer número
+            Console.Write("Ingrese el primer número: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            numMayor = num;
+
+            // Segundo número
+            Console.Write("Ingrese el segundo número: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            if (num > numMayor)
+            {
+                numMayor = num;
+            }
+
+            // Tercer número
+            Console.Write("Ingrese el tercer número: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            if (num > numMayor)
+            {
+                numMayor = num;
+            }
+
+            // Cuarto número
+            Console.Write("Ingrese el cuarto número: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            if (num > numMayor)
+            {
+                numMayor = num;
+            }
+
+            // Quinto número
+            Console.Write("Ingrese el quinto número: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            if (num > numMayor)
+            {
+                numMayor = num;
+            }
+
+            // Resultado
+            Console.WriteLine("El mayor es: " + numMayor);
+        }
+    }
+}
